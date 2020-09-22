cd499755d09cfa986228099ddb07a04fdafd07b9
suhas pb <suhas.380@gmail.com>
2020-09-22 11:56:27 -0500

    Update process-ad-lists.sh

--- process-ad-lists.sh
+++ process-ad-lists.sh
@@ -31,8 +31,8 @@ done
 # Remove lines starting with "#"
 sed -i '/^#/ d' blocked.list
 
-# Remove 127.0.0.1, 0.0.0.0, ::, ^M and leading white spaces
-sed -i -e 's/^127.0.0.1//' -e 's/^0.0.0.0//' -e '/::/d' -e 's/\r//g' -e "s/^[ \t]*//" blocked.list
+# Remove 127.0.0.1, 0.0.0.0, ::, ^ at the end of lines, || at the beginning, ^M and leading white spaces
+sed -i -e 's/^127.0.0.1//' -e 's/^0.0.0.0//' -e '/::/d' -e 's/\^//' -e 's/^||//' -e 's/\r//g' -e "s/^[ \t]*//" blocked.list
 
 # Remove duplicate lines
 perl -i -ne 'print if ! $a{$_}++' blocked.list
